6499f622d840610655a5517b4c3e436368f72743
Iryna Aleksandrova <ivalexie@ukr.net>
2022-12-15 12:57:44 +0200

    Update app.js
    add burger menu scroll

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Бургер меню
 const burgerMenu = document.querySelector('.menu-burger');
+const menuBody = document.querySelector('.menu-body');
 if (burgerMenu) {
-    const menuBody = document.querySelector('.menu-body');
     burgerMenu.addEventListener("click", function() {
         document.body.classList.toggle('_lock')
         burgerMenu.classList.toggle('active');
@@ -10,3 +10,29 @@ if (burgerMenu) {
 }
 
 // Прокрутка
+const menuLinks = document.querySelectorAll('.menu-link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - 30 - document.querySelector('.header').offsetHeight;
+
+            if(burgerMenu.classList.contains('active')) {
+                document.body.classList.remove('_lock')
+                burgerMenu.classList.remove('active');
+                menuBody.classList.remove('active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
